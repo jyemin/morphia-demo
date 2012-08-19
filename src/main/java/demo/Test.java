6e8e49ee6bcab8b43299644bd3285b889931e1bb
Jeff Yemin <jeff.yemin@10gen.com>
2012-08-19 17:02:46 -0400

    First entity

--- src/main/java/demo/Test.java
+++ src/main/java/demo/Test.java
@@ -8,7 +8,17 @@ public class Test {
     public static void main(String[] args) throws Exception {
         Morphia morphia = new Morphia();
         Mongo mongo = new Mongo();
+        mongo.dropDatabase("test");
         Datastore ds = morphia.createDatastore(mongo, "test");
+
+        Programmer programmer = new Programmer();
+        programmer.name= "Jeff";
+
+        ds.save(programmer);
     }
+}
+
+class Programmer {
+    String name;
 }
 
