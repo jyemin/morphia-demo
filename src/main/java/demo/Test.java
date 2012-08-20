6e8d45560ee65aec3f51006e4646e9bcbd91b3ea
Jeff Yemin <jeff.yemin@10gen.com>
2012-08-19 22:36:27 -0400

    Added Organization entity.  Fixed Repository entity to use abstract Member base class forowner and Repository reference for forkedFrom

--- src/main/java/demo/Test.java
+++ src/main/java/demo/Test.java
@@ -2,9 +2,9 @@ package demo;
 
 import com.google.code.morphia.Datastore;
 import com.google.code.morphia.Morphia;
-import com.google.code.morphia.annotations.Embedded;
 import com.google.code.morphia.annotations.Entity;
 import com.google.code.morphia.annotations.Id;
+import com.google.code.morphia.annotations.Property;
 import com.google.code.morphia.annotations.Reference;
 import com.mongodb.Mongo;
 import org.bson.types.ObjectId;
@@ -17,45 +17,56 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) throws Exception {
         Morphia morphia = new Morphia();
+        morphia.map(Repository.class).map(Programmer.class);
         Mongo mongo = new Mongo();
         mongo.dropDatabase("test");
         Datastore ds = morphia.createDatastore(mongo, "test");
+        ds.ensureIndexes();
 
-        Programmer programmer = new Programmer();
-        programmer.githubUserName = "scotthernandez";
-        programmer.name = new Name("Scott", "Hernandez");
-        programmer.memberSince = SimpleDateFormat.getDateInstance().parse("Aug 12, 2009");
-        programmer.active = true;
-        programmer.followers = 8;
-        programmer.following = Arrays.asList("moraes", "stickfigure");
-        ds.save(programmer);
+        Programmer scott = new Programmer();
+        scott.userName = "scotthernandez";
+        scott.name = "Scott Hernandez";
+        scott.since = SimpleDateFormat.getDateInstance().parse("Aug 12, 2009");
+        scott.active = true;
+        scott.followers = 8;
+        scott.following = Arrays.asList("moraes", "stickfigure");
+        ds.save(scott);
 
-        System.out.println(programmer);
+        System.out.println(scott);
 
-        Repository repo = new Repository(programmer, "docs", "mongodb/docs");
+        Organization mongodb = new Organization();
+        mongodb.userName = "mongodb";
+        mongodb.name = "mongodb";
+        mongodb.since = SimpleDateFormat.getDateInstance().parse("Jan 8, 2009");
+        ds.save(mongodb);
 
-        ds.save(repo);
+        Repository mongoDocs = new Repository(mongodb, "docs");
+        ds.save(mongoDocs);
 
-        System.out.println(repo);
+        Repository scottDocs = new Repository(scott, "docs", mongoDocs);
+        ds.save(scottDocs);
     }
 }
 
-@Entity("programmers")
-class Programmer {
-    @Id
-    String githubUserName;
-    Name name;
-    Date memberSince;
+@Entity
+abstract class Member {
+    @Id String userName;
+    @Property("memberSince") Date since;
     boolean active;
+    String name;
+}
+
+@Entity("programmers")
+class Programmer extends Member {
     int followers;
     List<String> following;
 
     @Override
     public String toString() {
         return "Programmer{" +
-                "githubUserName='" + githubUserName + '\'' +
+                "userName='" + userName + '\'' +
                 ", name='" + name + '\'' +
-                ", memberSince=" + memberSince +
+                ", since=" + since +
                 ", active=" + active +
                 ", followers=" + followers +
                 ", following=" + following +
@@ -63,24 +74,33 @@ class Programmer {
     }
 }
 
-@Embedded
-class Name {
-    String first, last;
-
-    Name(final String first, final String last) {
-        this.first = first;
-        this.last = last;
+@Entity("orgs")
+class Organization extends Member {
+    @Override
+    public String toString() {
+        return "Programmer{" +
+                "userName='" + userName + '\'' +
+                ", name='" + name + '\'' +
+                ", since=" + since +
+                ", active=" + active +
+                '}';
     }
 }
 
 @Entity("repos")
 class Repository {
     @Id ObjectId id;
-    @Reference Programmer owner;
+    @Reference Member owner;
     String name;
-    String forkedFrom;
+    @Reference Repository forkedFrom;
 
-    Repository(final Programmer owner, final String name, final String forkedFrom) {
+    Repository() {}
+
+    public Repository(final Member owner, final String name) {
+        this(owner, name, null);
+    }
+
+    Repository(final Member owner, final String name, final Repository forkedFrom) {
         this.owner = owner;
         this.name = name;
         this.forkedFrom = forkedFrom;
